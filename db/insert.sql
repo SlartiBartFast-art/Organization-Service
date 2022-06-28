898221128cf2a848dd29f3a04a3f5c18b800e0ee
SlartiBartFast-art <JekHamond@tutanota.com>
2022-06-28 20:26:26 +0500

    commit 8.0 update insert.sql

--- db/insert.sql
+++ db/insert.sql
@@ -1,2 +1,3 @@
 insert into organizations(organization_id, name, contact_name, contact_email, contact_phone)
-VALUES ('optimaGrowth', 'optimaGrowth', 'Jona Hill', 'optima@post.com', 5555789456);
+VALUES ('optimaGrowth', 'optimaGrowth', 'Jona Hill', 'optima@post.com', 5555789456),
+       ('VizusGrowth', 'optimaGrowth', 'Patrik Jorah', 'vizusOsp@post.com', 7355789456);
